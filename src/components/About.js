64b295e8387641ceec4fdbfc1936ca75f4f1cca1
haider <haiderlek@gmail.com>
2021-07-25 23:11:16 +0300

    commit tree c84c8e83e8b5a3293d6da19823c81c6878ce4ad7

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const bio = function () {
+    if (!props.bio || props.bio === "") {
+      return;
+    } else return <p>{props.bio}</p>;
+  };
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
